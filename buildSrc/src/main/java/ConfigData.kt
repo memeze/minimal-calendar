bbceff5232090109bad49cbcb0e8b71af3f4dff4
Minjae Kim <alswo5052@gmail.com>
2022-05-04 14:15:33 +0900

    chore: Change target sdk version

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,8 +1,8 @@
 object ConfigData {
     const val APPLICATION_ID = "com.memeze.minimalcalendar"
-    const val COMPILE_SDK = 32
+    const val COMPILE_SDK = 31
     const val MIN_SDK = 21
-    const val TARGET_SDK = 32
+    const val TARGET_SDK = 31
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 }
