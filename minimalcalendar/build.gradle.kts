ab258183663f6834e12005ed1a4fa0f209e6199f
Minjae Kim <alswo5052@gmail.com>
2022-05-11 17:32:12 +0900

    chore: Update version (1.0.8)

--- minimalcalendar/build.gradle.kts
+++ minimalcalendar/build.gradle.kts
@@ -85,7 +85,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.memeze"
                 artifactId = "minimalcalendar"
-                version = "1.0.7"
+                version = "1.0.8"
             }
         }
     }
