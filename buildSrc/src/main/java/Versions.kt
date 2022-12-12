cd7822570154b02bbf5f5075c3f912070886550b
memeze <alswo5052@gmail.com>
2022-12-12 09:37:06 +0900

    chore: Update compose version

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val ACCOMPANIST = "0.25.0"    // Used in compose 1.1.x version
     const val ACTIVITY = "1.5.1"
     const val APPCOMPAT = "1.4.2"
-    const val COMPOSE = "1.2.0"
+    const val COMPOSE = "1.3.2"
     const val CORE_KTX = "1.8.0"
     const val GRADLE_PLUGIN = "7.1.3"
     const val JUNIT = "4.13.2"
